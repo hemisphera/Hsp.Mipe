445d2c48e2773b5bbef957b5803ec614c3c56d6b
The Dumb <info@hemisphera.com>
2024-08-10 22:51:50 +0200

    wip

---
+++ Hsp.MidiProxy/Arguments.cs
@@ -0,0 +1,12 @@
+﻿using CommandLine;
+
+namespace Hsp.MidiProxy;
+
+public class Arguments
+{
+  public static Arguments Instance { get; set; }
+
+
+  [Option('e', "enable", Required = false, HelpText = "Enable the MIDI proxy.")]
+  public bool AutoEnable { get; set; }
+}
